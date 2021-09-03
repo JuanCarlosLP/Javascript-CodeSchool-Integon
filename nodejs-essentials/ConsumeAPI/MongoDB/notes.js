410f5e8de21c6744d9163c83001e7684061ebde3
JuanCarlosLP <jclarios.js@gmail.com>
2021-09-03 22:14:36 +0000

    Fixing call

--- nodejs-essentials/ConsumeAPI/MongoDB/notes.js
+++ nodejs-essentials/ConsumeAPI/MongoDB/notes.js
@@ -1,58 +1,45 @@
 $(document).ready(despliegaNotas);
 
 function despliegaNotas() {
-  fetch('http://localhost:3000/notes/')
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json);
-
-      json.forEach((note) => {
-        $('#notas-container').append(
-          `
-       <div class="card" style="width: 18rem;">
-            <div class="card">
-              <h5 class="card-header card-title">${note.title}</h5>
-              <div class="card-body">
-              <div class="card-subtitle mb-2 text-muted">id: ${note._id}</div>
-              <p class="card-text  text-success">${note.content}</p>
-              <a href="#" class="card-link">Edit</a>
-              <a href="#" class="card-link">Delete</a>
-              </div>
-            </div>
-          </div>
-        `
-        );
-      });
-    });
+    fetch("http://localhost:3000/notes/")
+        .then((response) => response.json())
+        .then((json) => {
+            json.forEach((note) => {
+                let nota =  `<tr>
+                                <td>${note.title}<td>
+                                <td>${note.content}<td>
+                                <td> <a href="#" class="card-link">Edit</a> <a href="#" class="card-link">Delete</a> <td>
+                            </tr>`;
+                $('#table1').append(nota);
+            });
+        });
 }
-$('#crearNota').click(function (event) {
-  event.preventDefault();
 
-  var titulo = $('#titulo').val();
-  var contenido = $('#contenido').val();
+$("#crearNota").click(function (event) {
+    event.preventDefault();
 
-  console.log(titulo, contenido);
+    var titulo = $("#titulo").val();
+    var contenido = $("#contenido").val();
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-  var myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-
-  var raw = JSON.stringify({
-    title: titulo,
-    content: contenido,
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow',
-  };
-
-  fetch('http://localhost:3000/notes/', requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log('error', error))
-    .finally(function () {
-      despliegaNotas();
+    var raw = JSON.stringify({
+        title: titulo,
+        content: contenido,
     });
+
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch("http://localhost:3000/notes/", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error))
+        .finally(function () {
+            despliegaNotas();
+        });
 });
